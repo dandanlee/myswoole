a6c1839b807cd4f875c8b96f71e4d7a85e16c65e
Dandan Lee <z9n5@outlook.com>
2017-04-17 14:56:15 +0800

    test

--- first.php
+++ first.php
@@ -1,2 +1,15 @@
 <?php
+/**
+ * 初始化swoole_server服务
+ */
 $serv = new swoole_server("127.0.0.1", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+
+/**
+ * 设置服务的参数
+ */
+$serv->set(array(
+    'worker_num' => 4,
+    'daemonize' => true,
+    'backlog' => 128,
+));
+
