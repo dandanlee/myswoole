9b8ecb3085cb9faf73ef05a7a7896e16b5e63c41
Dandan Lee <z9n5@outlook.com>
2017-04-17 16:21:12 +0800

    tcp server

--- first.php
+++ first.php
@@ -1,15 +1,21 @@
 <?php
-/**
- * 初始化swoole_server服务
- */
-$serv = new swoole_server("127.0.0.1", 9501, SWOOLE_BASE, SWOOLE_SOCK_TCP);
+// server对象，监听127.0.0.1:9501
+$serv = new swoole_server("127.0.0.1", 9501);
 
-/**
- * 设置服务的参数
- */
-$serv->set(array(
-    'worker_num' => 4,
-    'daemonize' => true,
-    'backlog' => 128,
-));
+//监听连接进入事件
+$serv->on('connect', function($serv, $fd){
+    echo "Client: Connect.\n";
+});
 
+//监听数据接收事件
+$serv->on('receive', function($serv, $fd, $from_id, $data){
+    $serv->send($fd, "Server: ".$data."\n");
+});
+
+//监听连接关闭事件
+$serv->on('close', function($serv, $fd){
+    echo "Client: close. \n";
+});
+
+//启动服务器
+$serv->start();
